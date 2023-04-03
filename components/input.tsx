afc8da87c25e47607f8c0423c3f1f99edd5450df
Guy Peer <peer.g2@gmail.com>
2023-04-03 16:13:33 +0300

    added interface for input

--- components/input.tsx
+++ components/input.tsx
@@ -1,7 +1,28 @@
-const Input = () => {
+import React from 'react'
+
+interface InputProps {
+    id: string
+    onChange: any
+    value: string
+    label: string
+    type?: string
+}
+
+const Input: React.FC<InputProps> = ({
+    id,
+    onChange,
+    value,
+    label,
+    type
+}) => {
     return (
-        <input className="
-block
+        <div className="relative">
+            <input id={id}
+                type={type}
+                value={value}
+                onChange={onChange}
+                className="
+block 
 rounded-md
 px-6
 pt-6
@@ -15,7 +36,13 @@ focus:outline-none
 focus:ring-0
 peer
 "
-placeholder=" "/>
+                placeholder=" " />
+            <label htmlFor={label}
+                className="absolute text-md text-zinc-400 duration-150 transform -translate-y-3 scale-75 top-4 z-10 origin-[0]
+            left-6 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus-translate-y-3 ">
+                Email
+            </label>
+        </div>
     )
 }
 
