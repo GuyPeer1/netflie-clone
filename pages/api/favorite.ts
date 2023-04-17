452a8811469d94f834cf6388dec4ff1a0e680652
Guy Peer <peer.g2@gmail.com>
2023-04-17 16:31:02 +0300

    created favorite api

---
+++ pages/api/favorite.ts
@@ -0,0 +1,69 @@
+import { NextApiRequest, NextApiResponse } from "next"
+import { without } from "lodash"
+import prismadb from '@/lib/prismadb'
+import serverAuth from "@/lib/serverAuth"
+import { error } from "console"
+
+export default async function (req: NextApiRequest, res: NextApiResponse) {
+    try {
+        if (req.method === 'POST') {
+            const { currentUser } = await serverAuth(req)
+
+            const { moveId } = req.body
+
+            const existingMovie = await prismadb.movie.findUnique({
+                where: {
+                    id: moveId
+                }
+            })
+            if (!existingMovie) {
+                throw new Error('Invalid Id')
+            }
+            const user = await prismadb.user.update({
+                where: {
+                    email: currentUser.email || ''
+                },
+                data: {
+                    favoriteIds: {
+                        push: moveId
+                    }
+                }
+            })
+            return res.status(200).json(user)
+        }
+
+        if (req.method === 'DELETE') {
+
+            const { currentUser } = await serverAuth(req)
+            const { moveId } = req.body
+            const existingMovie = await prismadb.movie.findUnique({
+                where: {
+                    id: moveId
+                }
+            })
+
+            if (!existingMovie) {
+                throw new Error('Invalid Id')
+            }
+
+            const updatedFavoriteIds = without(currentUser.favoriteIds, moveId)
+
+            const user = await prismadb.user.update({
+                where: {
+                    email: currentUser.email || ''
+                },
+                data: {
+                    favoriteIds: updatedFavoriteIds
+                }
+            })
+            return res.status(200).json(user)
+
+        }
+
+        return res.status(405).end()
+
+    } catch (error) {
+        console.log(error)
+        return res.status(400).end()
+    }
+}
